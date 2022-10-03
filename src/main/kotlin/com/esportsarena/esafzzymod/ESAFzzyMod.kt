eb1eed0e44ea14b2303d8401ad614b27b7f8136d
Fzzy <fzzysoda@gmail.com>
2022-10-03 13:41:30 -0700

    asdfnuiasdfghidfhg

--- src/main/kotlin/com/esportsarena/esafzzymod/ESAFzzyMod.kt
+++ src/main/kotlin/com/esportsarena/esafzzymod/ESAFzzyMod.kt
@@ -18,12 +18,26 @@ object ESAFzzyMod : ModInitializer {
 
     private var currentTime = 0L
     private var timerRunning = true
-    override fun onInitialize() {
-        println("Example mod has been initialized.")
+    private var showTimer = true
 
+    private var lastWorldSeed = 0L
+    override fun onInitialize() {
         ClientTickEvents.START_WORLD_TICK.register(ClientTickEvents.StartWorldTick { world ->
             run {
-                if (world.time < 20) timerRunning = true
+                val server = MinecraftClient.getInstance().server
+                if (server != null) {
+                    val seed = server.worlds.first().seed
+                    if (lastWorldSeed != seed) {
+                        if (world.time >= 20) {
+                            showTimer = false
+                        }
+                    }
+                    lastWorldSeed = seed
+                }
+                if (world.time < 20) {
+                    timerRunning = true
+                    showTimer = true
+                }
                 if (timerRunning) currentTime = world.time - (20 * 2)
                 for (item in MinecraftClient.getInstance().player!!.inventory.main) {
                     if (item.registryEntry.key.get().value.toString() == "minecraft:diamond") {
@@ -35,10 +49,27 @@ object ESAFzzyMod : ModInitializer {
 
         HudRenderCallback.EVENT.register(HudRenderCallback { matrixStack, tickDelta ->
             run {
-                val client = MinecraftClient.getInstance()
-                matrixStack.push()
-                matrixStack.scale(2F, 2F, 2F)
-                val size =
+                if (showTimer) {
+                    val client = MinecraftClient.getInstance()
+                    matrixStack.push()
+                    matrixStack.scale(2F, 2F, 2F)
+                    val size =
+                        client.textRenderer.draw(
+                            matrixStack,
+                            TimeFormat.timerFormat(currentTime * 50, timerRunning),
+                            27F,
+                            10F,
+                            if (timerRunning) Color.WHITE.rgb else Color.GREEN.rgb
+                        )
+                    DrawableHelper.fill(matrixStack, 0, 0, 10 + size, 27, Color(0, 0, 0, 100).rgb)
+                    matrixStack.scale(0.5F, 0.5F, 0.5F)
+
+                    RenderSystem.setShaderFogColor(1F, 1F, 1F, 1F)
+                    client.textureManager.bindTexture(Identifier(MOD_ID, "textures/gui/esalogo.png"))
+                    RenderSystem.setShaderTexture(0, Identifier(MOD_ID, "textures/gui/esalogo.png"))
+                    DrawableHelper.drawTexture(matrixStack, 10, 10, 0F, 0F, 32, 35, 32, 35)
+
+                    matrixStack.scale(2F, 2F, 2F)
                     client.textRenderer.draw(
                         matrixStack,
                         TimeFormat.timerFormat(currentTime * 50, timerRunning),
@@ -46,23 +77,8 @@ object ESAFzzyMod : ModInitializer {
                         10F,
                         if (timerRunning) Color.WHITE.rgb else Color.GREEN.rgb
                     )
-                DrawableHelper.fill(matrixStack, 0, 0, 10 + size, 27, Color(0, 0, 0, 100).rgb)
-                matrixStack.scale(0.5F, 0.5F, 0.5F)
-
-                RenderSystem.setShaderFogColor(1F, 1F, 1F, 1F)
-                client.textureManager.bindTexture(Identifier(MOD_ID, "textures/gui/esalogo.png"))
-                RenderSystem.setShaderTexture(0, Identifier(MOD_ID, "textures/gui/esalogo.png"))
-                DrawableHelper.drawTexture(matrixStack, 10, 10, 0F, 0F, 32, 35, 32, 35)
-
-                matrixStack.scale(2F, 2F, 2F)
-                client.textRenderer.draw(
-                    matrixStack,
-                    TimeFormat.timerFormat(currentTime * 50, timerRunning),
-                    27F,
-                    10F,
-                    if (timerRunning) Color.WHITE.rgb else Color.GREEN.rgb
-                )
-                matrixStack.pop()
+                    matrixStack.pop()
+                }
             }
         })
     }
